54e345edee4794494b7bb9daab7522ae9ffb750c
Aastik <aastikdude7@gmail.com>
2024-02-03 23:50:04 +0530

    defined attack

--- character.py
+++ character.py
@@ -1,2 +1,9 @@
 class Characters:
     def __init__(self, name:str , health:int, damage:int):
+        self.name = name
+        self.health = health
+        self.damage = damage
+
+    def attack(self, target) -> None:
+        target.hp -= self.damage
+        target.hp = max(target.hp, 0)
