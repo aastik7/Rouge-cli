bfa8d7c4373cc9a9f2a17d33368721c43a7cdd94
Aastik <aastikdude7@gmail.com>
2024-02-04 22:57:07 +0530

    iron sword weapon type

--- weapon.py
+++ weapon.py
@@ -4,4 +4,15 @@ class Weapon:
         self.weapon_type = weapon_type
         self.damage = damage 
         self.value = value
+        self.name = name 
+
         self.name = name
+        self.weapon_type = weapon_type
+        self.value = value
+
+iron_sword = Weapon(name="Iron Sword",
+                    weapon_type="sharp",
+                    damage = 5,
+                    value = 10)
+
+short_bow = Weapon(n)
