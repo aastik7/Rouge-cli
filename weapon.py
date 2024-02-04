02f29d6d26c1a81bd2c8c98658e4be50a77126e5
Aastik <aastikdude7@gmail.com>
2024-02-04 22:33:57 +0530

    weapon.py added

---
+++ weapon.py
@@ -0,0 +1,7 @@
+class Weapon:
+    def __init__(self, name: str, weapon_type: str, damage : str , value : int) -> None:
+        self.name = name
+        self.weapon_type = weapon_type
+        self.damage = damage 
+        self.value = value
+        self.name = name
